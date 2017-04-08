7fe86fffa06fc23d4bec52c2a5077e23c552c879
sunilkhokalay <sunilkhokalay@gmail.com>
2017-04-08 20:43:10 +0530

    Adding first unit test python script

---
+++ unitTest.py
@@ -0,0 +1,20 @@
+import unittest,logging,os
+log = logging.getLogger(__name__)
+cwd = os.getcwd()
+logging.basicConfig(filename="C:\git\myProject\log.txt",level=logging.DEBUG,)
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        log.info("This is the Setup section!!")
+
+    def test_Test1(self):
+        log.info("This is the test step 1")
+
+    def test_Test2(self):
+        log.info("This is the test step 2")
+
+    def tearDown(self):
+        log.info("This is the Tear down section!!")
+
+if __name__ == '__main__':
+    unittest.main()
